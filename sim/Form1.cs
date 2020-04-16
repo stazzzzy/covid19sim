ba2acf121bacaa158ebb1d30e7b30b7ec288afdd
Tristan <stazzzzy@gmail.com>
2020-04-16 16:02:51 -0400

    Initial commit

---
+++ sim/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        Person[] people;
+        int sample_size;
+        int infected, symptoms, recovered, healthy, dead;
+        int day = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Button action
+        {
+            sample_size = Int32.Parse(textBox1.Text); //Get integer from text box
+            simulate();
+        }
+
+        private void clicked_sample_size(object sender, EventArgs e) //Textbox enter action
+        {
+            if (textBox1.Text == "Sample Size")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Blue;
+            }
+        }
+
+        private void exit_sample_size(object sender, EventArgs e) //Textbox leave action
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Sample Size";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+        private void simulate()
+        {
+            people = new Person[sample_size];
+            healthy = sample_size;
+            for(int i=0;i<sample_size;i++)
+            {
+                people[i] = new Person(i,false,rnd.Next(1,100));
+                //Console.WriteLine("Person #" + i + " created!");
+            }
+            people[0].infect(); //Infect the first person
+        }
+        private void next_day()
+        {
+            infected = 0; symptoms = 0; recovered = 0; healthy = sample_size; dead = 0;
+            for (int i=0;i<sample_size;i++)
+            {
+                var names = people[i].Advance();
+                infected += names.Item1;
+                symptoms += names.Item2;
+                recovered += names.Item3;
+                healthy += names.Item4;
+                dead += names.Item5;
+            }
+            spread_infection();
+            Console.WriteLine("Infected:" + infected + "\nDead:" + dead + "\nHealthy:" + healthy + "\n");
+        }
+        private void spread_infection()
+        {
+            for(int i = 0; i < infected * 2; i++)
+            {
+                int index = rnd.Next(0, people.Length);
+                if(!people[index].is_dead() && !people[index].is_recovered())
+                {
+                    people[index].infect();
+                }
+            }
+        }
+
+        private void next_day_clicked(object sender, EventArgs e) //Button action
+        {
+            next_day();
+            chart.Series[0].Points.AddXY(day, healthy);
+            chart.Series[1].Points.AddXY(day, infected);
+            chart.Series[2].Points.AddXY(day, dead);
+            day += 1;
+        }
+    }
+}
---
+++ sim/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Person
+    {
+        private readonly int id;
+        Boolean infected, symptoms, recovered, dead;
+        int days_since_infection = 0;
+        int seed;
+
+        public Person(int i, Boolean x, int s)
+        {
+            id = i;
+            seed = s; //op
+            infected = false;
+            symptoms = false;
+            recovered = false;
+            dead = false;
+            days_since_infection = 0;
+        }
+        public Boolean Is_infected()
+        {
+            return infected;
+        }
+        public Boolean is_dead()
+        {
+            return dead;
+        }
+        public Boolean is_recovered()
+        {
+            return recovered;
+        }
+        public void infect()
+        {
+            infected = true;
+        }
+        public Tuple<int,int,int,int,int> Advance()
+        {
+            Random rnd = new Random();
+            if (!dead && infected)
+            {
+                days_since_infection += 1;
+                if (!symptoms && days_since_infection >= 5)
+                {
+                    symptoms = true;
+                }
+                else if (days_since_infection >= 15)
+                {
+                    if (seed <= 9)
+                    {
+                        dead = true;
+                        infected = false;
+                        symptoms = false;
+                    }
+                    else
+                    {
+                        infected = false;
+                        symptoms = false;
+                        recovered = true;
+                    }
+                }
+            }
+            if (infected && symptoms)
+            {
+                return Tuple.Create(1,1,0,-1,0);
+            }
+            else if (dead)
+            {
+                return Tuple.Create(0,0,0,-1,1);
+            }
+            else if (recovered)
+            {
+                return Tuple.Create(0,0,1,0,0);
+            }
+            else if (infected)
+            {
+                return Tuple.Create(1,0,0,-1,0);
+            }
+            else
+            {
+                return Tuple.Create(0,0,0,0,0);
+            }
+        }
+    }
+}
